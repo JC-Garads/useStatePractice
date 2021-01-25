954325eba813441755a151cfd2b14dd144d0cae3
JC-Garads <74990063+JC-Garads@users.noreply.github.com>
2021-01-25 13:53:52 +0800

    added my answers

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  setInterval(showTime, 1000);
+
+  let time = new Date().toLocaleTimeString();
+  const [press, setPress] = useState(time);
+  console.log(time);
+
+  function showTime() {
+    const newTime = new Date().toLocaleTimeString();
+    setPress(newTime);
+  }
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{press}</h1>
+      <button onClick={showTime}>Get Time</button>
     </div>
   );
 }
